34d5ef8afc14bbc2789c307cb0f5d43440c769cb
PedroH <pedrohsl1697@gmail.com>
2019-11-01 22:22:44 -0300

    listas de exercicios
    listas feitas na aula de algoritmo

---
+++ lista3Algoritimos/Lista3ex3.js
@@ -0,0 +1,19 @@
+
+    <script language=javascript>
+        num1=parseInt(prompt("digite um numero"))
+        num2=parseInt(prompt("digite um segundo numero"))
+        num3=parseInt(prompt("digite um terceiro numero"))
+        if (num1>num2 && num1>num3)
+        {
+            alert("o maior numero e "+num1)   
+        }
+        else if (num2>num1 && num2>num3)
+        {
+            alert("o maior numero e "+num2)
+        }
+        else if (num3>num1 && num3>num2)
+        {
+            alert("o maior numero e "+num3)
+        }
+
+    </script>
